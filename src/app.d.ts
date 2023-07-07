bb3420f267f2693f6f85ba122563ea38eac756d3
Chef <cheftech@patraldo.dev>
2023-07-06 19:55:54 -0600

    test rename of repository

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,7 +5,14 @@ declare global {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		// interface Platform {}
+	           interface Platform {
+      env: {
+        DB: D1Database;
+      };
+      context: {
+        waitUntil(promise: Promise<any>): void;
+      };
+      caches: Cache
 	}
 }
 
---
+++ src/routes/temp/+page.svelte
@@ -0,0 +1,1 @@
+<h1> Test repository rename </h1>
